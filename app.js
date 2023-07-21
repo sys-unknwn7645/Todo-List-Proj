24bce1f2c23a6b6c073f54c537022d7ece92fed7
Bhushan Pawar <sys.unknwn7645@gmail.com>
2023-07-21 15:10:55 -0400

    updated app.listen port

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ async function main (){
 };
 //end of main()
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("server started on port 3000")
 });
 
